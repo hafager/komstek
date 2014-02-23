26fab1a1415f154caee916f2cc51819478143b40
Håvard Magne Fagervoll <fagervoll@gmail.com>
2014-02-23 07:23:59 +0100

    Make progress bar work

--- dist/js/percent_siving.js
+++ dist/js/percent_siving.js
@@ -1,15 +1,14 @@
-<script>
 function percent_siving(){
 
-var now = new Date();
+        var now = new Date();
 
-var start = new Date(12, 8, 15);
-var end = new Date(17,6,1);
+        var start = new Date(14, 2, 15);
+        var end = new Date(17,6,1);
 
-var newnow = now-start;
-var total = end-start;
+        var newnow = now-start;
+        var total = end-start;
 
-var percent = Math.round(newnow/total);
-return percent.toString();
-}
-</script>
+        var percent = newnow/total;
+        var progress = percent.toString().concat("%");
+        document.getElementById("progress123").style.width=progress;
+      }
